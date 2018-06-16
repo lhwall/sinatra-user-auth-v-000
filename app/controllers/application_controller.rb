96d4cca1654ade0bb96e222386af6792fef3335a
Lucy Wall <lhwall@gmail.com>
2018-06-16 00:59:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id    redirect '/users/home'
+@user = User.find_by(email: params["email"]), password: params["password"])
+ session[:id] = @user.id
+    redirect '/users/home'
   end
 
   get '/sessions/logout' do
